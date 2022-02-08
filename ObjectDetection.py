887f6f45b2c3850a818d46036ef4386565979e9b
Manvendra Shekhawat <66855354+manvendra16@users.noreply.github.com>
2022-02-08 11:21:56 +0530

    Update ObjectDetection.py

--- ObjectDetection.py
+++ ObjectDetection.py
@@ -1,10 +1,15 @@
 import tensorflow as tf
-import numpy as np
+from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
-import cv2
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
-import os
+import numpy as np
+
+IMG_HEIGHT = 400
+IMG_WIDTH = 600
+BATCH_SIZE = 20
+CHANNELS = 3
+EPOCHS = 50
+
+# callback class
 
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
@@ -13,59 +18,108 @@ class myCallback(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 callbacks = myCallback()
 
-def load_images_from_folder(folder):
-    images = []
-    for filename in os.listdir(folder):
-        img = cv2.imread(os.path.join(folder,filename))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        if img is not None:
-            images.append(cv2.resize(img,(600,400),3))
-    return images
+# input
 
-train = ImageDataGenerator(rescale=1/255)
-validation = ImageDataGenerator(rescale=1/255)
+dataset = tf.keras.preprocessing.image_dataset_from_directory(r'D:\CODES\Python Projects\Files\Tensorflow\wheat_leaf',
+                                                    shuffle=True,
+                                                    image_size=(IMG_WIDTH,IMG_HEIGHT),
+                                                    batch_size= BATCH_SIZE
+                                                    )
 
-training = train.flow_from_directory(r'C:\Users\91979\Downloads\Dataset\Training',
-                                    target_size= (400,600),
-                                    batch_size= 10,
-                                    class_mode= 'categorical')
-validating = validation.flow_from_directory(r'C:\Users\91979\Downloads\Dataset\Validation',
-                                    target_size= (400,600),
-                                    batch_size= 10,
-                                    class_mode= 'categorical')
+n_classes = len(dataset.class_names)
+class_names = dataset.class_names
 
-model = tf.keras.models.Sequential([
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(400,600,3)),
-        tf.keras.layers.MaxPooling2D(2,2),
-        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(2,2),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(units=64, activation=tf.nn.relu),
-        tf.keras.layers.Dense(units=3, activation=tf.nn.softmax)
+# for image_batch, label_batch in dataset.take(1):
+#    print(image_batch[0].numpy())
+
+# image visualisation
+
+# plt.imshow(image_batch[0].numpy().astype('uint8'))
+# plt.title(dataset.class_names[label_batch[0]])
+# plt.axis('off')
+
+# train test split
+
+def get_dataset_partiions(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+  
+      ds_size = len(ds)
+    
+      if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+      train_size = int(train_split * ds_size)
+      val_size = int(val_split * ds_size)
+    
+      train_ds = ds.take(train_size)
+    
+      val_ds = ds.skip(train_size).take(val_size)
+      test_ds = ds.skip(train_size).skip(val_size)
+    
+      return train_ds, val_ds, test_ds
+
+train, validation, test = get_dataset_partiions(dataset)
+
+# Optimizing the dataset by using cache and prefetch
+
+train = train.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+validation = validation.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test = test.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+# preprocessing
+
+rescale_and_resize = tf.keras.Sequential([
+                     layers.experimental.preprocessing.Resizing(IMG_WIDTH, IMG_HEIGHT),
+                     layers.experimental.preprocessing.Rescaling(1.0/255)
 ])
 
-model.summary()
+data_augmentation = tf.keras.Sequential([
+                                         layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+                                         layers.experimental.preprocessing.RandomRotation(0.2)
+])
 
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model = models.Sequential([
+                          rescale_and_resize,
+                          data_augmentation,
+                          layers.Conv2D(32, (3,3), activation='relu', input_shape=(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, CHANNELS)),
+                          layers.MaxPooling2D((2,2)),
+                          layers.Conv2D(64, (3,3), activation='relu'),
+                          layers.MaxPooling2D((2,2)),
+                          layers.Conv2D(64, (3,3), activation='relu'),
+                          layers.MaxPooling2D((2,2)),
+                          
+                          layers.Flatten(),
+                          layers.Dense(64, activation='relu'),
+                          layers.Dense(n_classes, activation='softmax')
+])
+ 
+model.build(input_shape=(BATCH_SIZE, IMG_HEIGHT, IMG_WIDTH, CHANNELS))
 
-model.fit(training, epochs=10, callbacks=[callbacks], validation_data=validating)
+# model.summary()
 
-# print(validating.class_indices)
+model.compile(
+    optimizer='adam',
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits=False),
+    metrics=['accuracy']
+)
 
-dir_path = r'C:\Users\91979\Downloads\Dataset\Testing'
-im = load_images_from_folder(dir_path)
+history = model.fit(
+    train,
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    verbose=1,
+    callbacks=[callbacks],
+    validation_data=val,
+)
 
-arr = [i.reshape(1,i.shape[0],i.shape[1],i.shape[2]) for i in im]
+scores = model.evaluate(test)
 
-out = [model.predict(i) for i in arr]
-
-for i in out:
-    if i[0][0] == 1:
-        print('Healthy')
-    elif i[0][1] == 1:
-        print('Septoria')
-    else:
-        print('Stripe Rust')
-        
+for image_batch, label_batch in dataset.take(1):
+    first_image = image_batch[0].numpy().astype('uint8')
+    first_label = label_batch[0].numpy()
+      
+    print('First image to predit:')
+    plt.imshow(first_image)
+    print("First image's actual label:", class_names[first_label])
+      
+    batch_prediction = model.predict(image_batch)
+    print('predicted label:', class_names[np.argmax(batch_prediction[0])])
